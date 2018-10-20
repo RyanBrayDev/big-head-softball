ab8af2456a4f7a3cab23e981d5696d7e3fc75941
Ryan Bray <c-ryan.bray@charter.com>
2018-10-20 09:05:00 -0600

    Build out players functionality

--- server/server.js
+++ server/server.js
@@ -1,30 +1,58 @@
+// SET UP THE SERVER
+// =============================================================================
 const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+// SET UP THE DATABASE
+// =============================================================================
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
-var assert = require('assert');
-
-const app = express();
 const dbUri = "mongodb+srv://big_head:Password1@cluster0-vf3fb.mongodb.net/big_head?retryWrites=true";
+const dbOptions = {useNewUrlParser: true};
 
-app.get('/api/teams', async (req, res) => {
+
+// INTERCEPT ALL ROUTES
+// =============================================================================
+router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "*");
+    // do logging
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('Processing request: ' + req.originalUrl + ' from: ') + req.ip;
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+// GET
+// =============================================================================
+router.get('/', function (req, res) {
+    res.json({message: 'Big Head Softball API. All rights reserved.'});
+});
+
+// GET TEAMS
+// =============================================================================
+router.get('/teams', async (req, res) => {
     try {
-        MongoClient.connect(dbUri, function (err, client) {
+        MongoClient.connect(dbUri, dbOptions, function (err, client) {
             if (err) {
                 console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+                res.send(err);
             }
             else {
                 console.log('Connected...');
                 const db = client.db("big_head");
                 const collection = db.collection('teams');
                 collection.find({}).toArray(function (err, docs) {
-                    assert.equal(err, null);
                     console.log("Found the following records");
                     console.log(JSON.stringify(docs))
                     client.close();
-                    res.status(200);
-                    res.send(JSON.stringify(docs));
+                    res.status(200).json(docs);
                 });
             }
         });
@@ -36,28 +64,29 @@ app.get('/api/teams', async (req, res) => {
     }
 });
 
-app.get('/api/teams/:teamId', async (req, res) => {
+// GET TEAM
+// =============================================================================
+router.get('/teams/:teamId', async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     try {
-        MongoClient.connect(dbUri, function (err, client) {
+        MongoClient.connect(dbUri, dbOptions, function (err, client) {
             if (err) {
                 console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+                res.send(err);
             }
             else {
                 console.log('Connected...');
                 const db = client.db("big_head");
                 const collection = db.collection('teams');
-                collection.findOne({ _id: ObjectId(req.param("teamId")) }, function (err, team) {
+                collection.findOne({_id: ObjectId(req.param("teamId"))}, function (err, team) {
                     if (err) {
                         console.log('Error occurred while finding the record...\n', err);
                     }
-                    assert.equal(err, null);
                     console.log("Found the following records");
                     console.log(JSON.stringify(team))
                     client.close();
-                    res.status(200);
-                    res.send(JSON.stringify(team));
+                    res.status(200).json(team);
                 });
             }
         });
@@ -69,8 +98,42 @@ app.get('/api/teams/:teamId', async (req, res) => {
     }
 });
 
-// app.get('/', (req, res) => {
-//     res.sendFile('/dist/spectrum-deployment-dashboard/index.html', {root: '.'});
-// });
+// PUT
+// =============================================================================
+router.put('/teams/:teamId', async (req, res) => {
+    try {
+        console.log('PUT Called!!');
+        let updatedTeam = null;
+        MongoClient.connect(dbUri, dbOptions, function (err, client) {
+            if (err) {
+                console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+            }
+            else {
+                console.log('Connected...');
+                const collection = client.db("big_head").collection('teams');
+                const team = req.body;
+                team._id = ObjectId(req.params.teamId);
+                console.log('Updating record: ' + team._id);
+                collection.replaceOne({_id: ObjectId(req.params.teamId)}, team, function (err, res) {
+                    if (err) {
+                        console.log('Error occurred while updating the record...\n', err);
+                        res.send(err);
+                    }
+                });
+                client.close();
+            }
+            console.log('Record updated: ' + req.params._id);
+            res.location('api/teams/' + req.params.teamId).status(200).send();
+        });
+    } catch (e) {
+        console.log("An unexpected error occured: " + e.toString());
+        res.status(500).send(e);
+    }
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.use('/api', router);
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port, () => console.log('Big Head Softball API ready on port ' + port));
